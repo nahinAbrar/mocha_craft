3c687897be94aa042bc64cc1e5db7442c90c27a2
Asm Nahin <asmnahin21@gmail.com>
2024-08-25 16:55:07 +0600

    About Done flex bug

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 "use client"
+import About from '@/components/About'
 import Explore from '@/components/Explore'
 import Hero from '@/components/Hero'
 import React from 'react'
@@ -19,6 +20,7 @@ const Home = () => {
     <div className="h-full overflow-x-hidden">
       <Hero />
       <Explore />
+      <About />
       {/*Temporary Div*/}
       <div className="h-[4000px]"></div>
     </div>
